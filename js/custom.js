5b142bd3396e60cc49ca170128aca9a3da494bf5
Jack Morris <jxmorris12@gmail.com>
2019-03-28 01:52:32 -0400

    change svg color and other stuff

--- js/custom.js
+++ js/custom.js
@@ -26,10 +26,48 @@ function initializeArrowIcons() {
 	$('#arrow-right').hide();
 }
 
+
+function replaceSvgs() {
+	$('img.svg').each(function(){
+	    var $img = $(this);
+	    var imgID = $img.attr('id');
+	    var imgClass = $img.attr('class');
+	    var imgURL = $img.attr('src');
+
+	    $.get(imgURL, function(data) {
+	        // Get the SVG tag, ignore the rest
+	        var $svg = $(data).find('svg');
+
+	        // Add replaced image's ID to the new SVG
+	        if(typeof imgID !== 'undefined') {
+	            $svg = $svg.attr('id', imgID);
+	        }
+	        // Add replaced image's classes to the new SVG
+	        if(typeof imgClass !== 'undefined') {
+	            $svg = $svg.attr('class', imgClass+' replaced-svg');
+	        }
+
+	        // Remove any invalid XML tags as per http://validator.w3.org
+	        $svg = $svg.removeAttr('xmlns:a');
+
+	        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+	        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+	            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+	        }
+
+	        // Replace image with new SVG
+	        $img.replaceWith($svg);
+
+	    }, 'xml');
+
+	});
+	}
+
 /* add hover arrows on desktop */
 
 // show arrows when user moves mouse within this % of frame width
 var arrowShowPerc = 0.35; 
+var arrowAnimationDur = 200; // ms
 
 // the last x position of the cursor, tracked so we can tell which direction the cursor is moving
 var lastMouseX = $(window).width()/2; 
@@ -64,8 +102,9 @@ function checkMouseCoords(event)  {
 $(window).mousemove(checkMouseCoords);
 
 function onReady() {
-	resizeTurnOfPhrase();
+	replaceSvgs();
 	initializeArrowIcons();
+	resizeTurnOfPhrase();
 }
 
 $(document).ready(onReady);
